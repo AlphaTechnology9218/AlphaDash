ceae41e01e87342580815a4d3cb4ff0293fe76e7
Alpha <alphatechjac@gmail.com>
2025-12-12 02:24:11 -0300

    fix: Corrigir conflitos de merge em postcss.config.js e eslint.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
->>>>>>> 8973f19aa724bf4cf9f086bb4279aefa353827aa
